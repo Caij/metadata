bfef9d52a099ee5327e5068c0ecc3b6491dfd82b
Caij <271945881@qq.com>
2024-07-19 15:08:08 +0800

    fix

--- src/main/java/org/jaudiotagger/x/CharsetDetectorCompat.java
+++ src/main/java/org/jaudiotagger/x/CharsetDetectorCompat.java
@@ -3,8 +3,12 @@ package org.jaudiotagger.x;
 import org.mozilla.universalchardet.UniversalDetector;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CharsetDetectorCompat {
+
+    private List<byte[]> caches = new ArrayList<>();
 
     UniversalDetector detector = new UniversalDetector();
 
@@ -15,14 +19,32 @@ public class CharsetDetectorCompat {
     public void handleData(byte[] buf, int offset, int length) {
         if (length == 0) return;
         if (buf.length == 0) return;
+
+        byte[] cache = new byte[length];
+        System.arraycopy(buf, offset, cache, 0, length);
+        caches.add(cache);
+
         detector.handleData(buf, offset, length);
     }
 
     public void end() {
+        int max = 10;
+        if (!caches.isEmpty()) {
+            while (!detector.isDone() && max > 0) {
+                for (byte[] c : caches) {
+                    detector.handleData(c);
+                }
+                max--;
+            }
+        }
         detector.dataEnd();
     }
 
     public Charset getDetectedCharset() {
         return CharsetDetectorUtil.format(detector.getDetectedCharset());
+    }
+
+    public boolean isDone() {
+        return detector.isDone();
     }
 }
